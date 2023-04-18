971e3c08d32ba42b1c97b3134c917295328ae996
odin5on <dbodn@uiowa.edu>
2023-04-18 12:36:29 -0500

    Final code after checkoff

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -12,8 +12,10 @@
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
 
+#define __DELAY_BACKWARD_COMPATIBLE__
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdio.h>
 #include "i2cmaster.h"
 #include "twimaster.c"
@@ -64,6 +66,15 @@ unsigned char convert_voltage(float v){
 	return val;
 }
 
+void write_voltage(uint8_t address, uint8_t channel, uint8_t value){
+	i2c_start(address);
+	i2c_write(channel);
+	i2c_write(value);
+	i2c_stop();
+}
+
+const int sine[64] = {128, 141, 153, 165, 177, 188, 199, 209, 219, 227, 234, 241, 246, 250 ,254 ,255 ,255 ,255 ,254 ,250 ,246 ,241 ,234, 227, 219, 209, 199, 188, 177, 165, 153, 141, 128, 115, 103, 91, 79, 68, 57, 47, 37, 29, 22, 15, 10, 6, 2 ,1 ,0, 1, 2 ,6 ,10 ,15, 22, 29, 37, 47 ,57 ,68 ,79, 91, 103, 115};
+
 int main(void)
 {
 
@@ -76,41 +87,34 @@ int main(void)
 	
 	i2c_init();
 	
-	unsigned char channel0 = 00000000;
-	unsigned char channel1 = 00000001;
-	unsigned char addr = 01011000;
-	unsigned char volt = 10000001;
+	uint8_t addr = 0b01011000;
 	
 	char c[20];
 	char command;
-	int channel, waveforms = 0;
+	int channel, numofwaves = 0;
 	float third = 0;
     while (1) 
     {
 		scanf("%19s", c);
-		sscanf(c, "%c,%d,%f,%d", &command, &channel, &third, &waveforms);
-		printf("%c , %d , %f , %d\n", command, channel, third, waveforms);
+		sscanf(c, "%c,%d,%f,%d", &command, &channel, &third, &numofwaves);
+		printf("%c , %d , %f , %d\n", command, channel, third, numofwaves);
 		if(command == 'G'){
 			printf("v=%.3f\n", read_adc());
 		}
 		else if(command == 'S'){
-			printf("%d\n",convert_voltage(third));
-			int a = i2c_start(addr);
-			printf("start success?: %d\n", a);
-			if(channel == 1){
-				a = i2c_write(channel1);
-				printf("command write success?: %d\n", a);
-			} else {
-				a = i2c_write(channel0);
-				printf("command write success?: %d\n", a);
-			}
-			a = i2c_write(0xFF);
-			printf("write voltage success?:%d\n", a);
-			i2c_stop();
-			printf("after stop\n");
+			int a = convert_voltage(third);
+			write_voltage(addr, channel, a);
+			printf("DAC channel %d set to %.2f V (%dd)\n",channel,third,a);
 		}
 		else if(command == 'W'){
-		
+			printf("Generating %d sine wave cycles with f=%.0f Hz on DAC channel %d\n", numofwaves, third, channel);
+			double delay = 1000.0/(72.0*third);
+			for(int i=0; i<numofwaves; i++){
+				for(int j=0; j<64; j++){
+					write_voltage(addr, channel, sine[j]);
+					_delay_ms(delay);
+				}
+			}
 		}
     }
 }
